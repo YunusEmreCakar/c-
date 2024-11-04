14c94a73f9362906e62d98774813d385b1b6c19d
Yunus Emre Cakar <yunusemrecakar11@gmail.com>
2024-11-04 21:48:05 +0300

    Proje dosyası ekle.

---
+++ c++/C++dersi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int en, boy, alan;
+	cout << "Alan hesaplama programina hosgeldiniz" << endl;
+	cout << "Lutfen eni giriniz :"; cin >> en ;
+	cout << "Lutfen boyu giriniz :"; cin >> boy ;
+	alan = en * boy;
+	cout << "Verdiginiz degerlere gore hesaplanan alan :" << alan; 
+	return 0; 
+}
